d1bd7b41506cecf6117914a2bde97dff327e1681
Robert Stryzhak <89968748+bob4inski@users.noreply.github.com>
2023-03-13 00:53:17 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 
 apt-get update 
-apt-get install -y curl openssh-server ca-certificates tzdata perl
-apt-get install postfix
-apt-get install mailutils
+apt-get install -y curl openssh-server ca-certificates tzdata perl postfix mailutils
+
 
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
 
@@ -12,15 +11,14 @@ echo "127.0.0.1 gitlab.local" >> /etc/hosts
 gitlab-ctl reconfigure
 
 
-useradd -m -s /bin/bash gituser
-passwd gituser
+useradd -m -p gituser -s /bin/bash gituser
+
 postconf -e "mydestination = gitlab.local,localhost.localdomain, localhost"
 postconf -e "inet_interfaces = all"
+
 service postfix restart
 
 
-apt install ./vagrant_2.2.9_x86_64.deb
-apt install virtualbox
 apt-get install python3-pip
 python3 -m pip install  ansible
 
